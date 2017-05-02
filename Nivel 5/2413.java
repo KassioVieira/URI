d44bed95f0ed940a426c886240020c36e9963538
KassioVieira <kassio.vieira7@gmail.com>
2017-05-02 10:07:57 -0400

    Busca na Internet solucionada

---
+++ Nivel 5/2413.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int cliques;
+		
+		cliques = in.nextInt();
+		
+		System.out.println((cliques * 2) * 2);
+	}
+
+}
