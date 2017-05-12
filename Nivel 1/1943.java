9d24adeb79263662a5529f9498850a6314521f02
KassioVieira <kassio.vieira7@gmail.com>
2017-05-12 09:28:17 -0400

    problema TopN solucionado

---
+++ Nivel 1/1943.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int k = in.nextInt();
+		if( k == 1){
+			System.out.println("Top 1");
+		}else if(k > 1 && k <=3){
+			System.out.println("Top 3");
+		}else if(k > 3 && k <=5){
+			System.out.println("Top 5");
+		}else if(k > 5 && k <=10){
+			System.out.println("Top 10");
+		}else if(k > 10 && k <=25){
+			System.out.println("Top 25");
+		}else if(k > 25 && k <=50){
+			System.out.println("Top 50");
+		}else if(k > 50 && k <=100){
+			System.out.println("Top 100");
+		}
+
+	}
+
+ 
+}
