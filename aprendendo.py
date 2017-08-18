aff71f592d148e4f3f8d2604ae14b53ea08f5297
KassioVieira <kassio.vieira7@gmail.com>
2017-08-18 10:26:17 -0400

    aprendedo python

---
+++ aprendendo.py
@@ -0,0 +1,42 @@
+print('variaveis')
+
+media = 10
+var_pi = 3.14
+print(var_pi)
+
+type(var_pi)
+
+aprovado =  True
+print(aprovado)
+
+#print(media)
+
+#----------------------------------------------------------------
+
+# == igualdade
+# > maior que
+# < menor que
+# != diferente
+# >= maior ou igual
+# <= menor ou igual
+
+v1 = 10
+v2 = 20
+
+print(v1 + v2)
+print(v1 - v2)
+print(v1 * v2)
+print(v2 / v1)
+print(v2 // v1)
+print(v2 % v1)
+print(2 ** 10)
+
+# operadores Lógicos
+#not -> não (negação)
+#and - > e  (conjunção)
+#or -> ou   (disjunção)
+
+#operador and
+
+print(True and False)
+#not -> and -> or
