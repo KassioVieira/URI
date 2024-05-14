c9be8aa1f86cb945b45e3880256b12fa141b7e76
Kássio Luz <kassio.vieira7@gmail.com>
2024-05-14 18:12:37 -0300

    Adjust 1943.java - Top N
    https://judge.beecrowd.com/pt/problems/view/1943

--- Nivel 1/1943.java
+++ Nivel 1/1943.java
@@ -1,27 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int k = in.nextInt();
-		if( k == 1){
-			System.out.println("Top 1");
-		}else if(k > 1 && k <=3){
-			System.out.println("Top 3");
-		}else if(k > 3 && k <=5){
-			System.out.println("Top 5");
-		}else if(k > 5 && k <=10){
-			System.out.println("Top 10");
-		}else if(k > 10 && k <=25){
-			System.out.println("Top 25");
-		}else if(k > 25 && k <=50){
-			System.out.println("Top 50");
-		}else if(k > 50 && k <=100){
-			System.out.println("Top 100");
-		}
-
-	}
-
- 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        String[] levels = {"Top 1", "Top 3", "Top 5", "Top 10", "Top 25", "Top 50", "Top 100"};
+        int[] boundaries = {1, 3, 5, 10, 25, 50, 100};
+        
+        for (int i = 0; i < boundaries.length; i++) {
+            if (k <= boundaries[i]) {
+                System.out.println(levels[i]);
+                break;
+            }
+        }
+    }
 }
